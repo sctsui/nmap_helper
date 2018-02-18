77914f8baa4f339ca14238adb11eb640a75feddd
Sabrina Tsui <sctsui@users.noreply.github.com>
2018-02-18 09:26:13 -0800

    IP input file

--- conversation.py
+++ conversation.py
@@ -7,7 +7,8 @@ from watson_developer_cloud import ConversationV1
 #########################
 
 userin = '';
-
+ip_addr = '';
+watson_input = open("ip_input.txt","w")
 conversation = ConversationV1(
     username = 'ed62bc7e-3c21-4a8a-8cd9-aefa4f9bc049',
     password = 'HsXuM4Ifqchf',
@@ -20,10 +21,12 @@ if os.getenv("conversation_workspace_id") is not None:
 response1 = conversation.message(workspace_id=workspace_id, input={})
 print(json.dumps(response1['output']['text'][0], indent=2))
 
+ip_addr = input("Enter an IP address: ")
+watson_input.write(ip_addr)
+
 while userin is not 'q':
     userin = input("type response or q to quit: ")
     response2 = conversation.message(workspace_id=workspace_id, input={'text': str(userin)})
     print(json.dumps(response2['output']['text'][0], indent=2))
 
-
-print("Bye!")
+watson_input.close()
