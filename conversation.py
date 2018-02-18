7deda555d34ad76e972a50afe78e2e1bdfc4644e
Sabrina Tsui <sctsui@users.noreply.github.com>
2018-02-18 09:54:37 -0800

    poc for demo

--- conversation.py
+++ conversation.py
@@ -7,7 +7,9 @@ from watson_developer_cloud import ConversationV1
 #########################
 
 userin = '';
-
+ip_addr = '';
+watson_input = ''
+nmap_out = open("nmap_out.txt","r")
 conversation = ConversationV1(
     username = 'ed62bc7e-3c21-4a8a-8cd9-aefa4f9bc049',
     password = 'HsXuM4Ifqchf',
@@ -20,10 +22,17 @@ if os.getenv("conversation_workspace_id") is not None:
 response1 = conversation.message(workspace_id=workspace_id, input={})
 print(json.dumps(response1['output']['text'][0], indent=2))
 
+ip_addr = input("Enter an IP address: ")
+watson_input = watson_input + ip_addr #watson_input.write(ip_addr)
+
 while userin is not 'q':
     userin = input("type response or q to quit: ")
+    if userin == "I entered the IP":
+        print (watson_input)
+    if userin == "Give me the raw nmap output":
+        for line in nmap_out:
+            print (line)
     response2 = conversation.message(workspace_id=workspace_id, input={'text': str(userin)})
     print(json.dumps(response2['output']['text'][0], indent=2))
-
-
-print("Bye!")
+    
+nmap_out.close()
